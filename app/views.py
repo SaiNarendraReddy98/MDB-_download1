c4191d42e8ec3a35c1143d4758c836179b1823d9
SaiNarendraReddy98 <sainarendra62645@gmail.com>
2023-11-29 22:36:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdb4(request):
+    return render(request,'mdb4.html')
+
+def home(request):
+    return render(request,'home.html')
